f3d5aca3c16062b6ec89a7d79f7b45b3eb625e57
Scott Robinson <you@yourdomain.com>
2017-01-13 10:54:38 -0600

    Added the bop it image, views and gesture recognizers

--- bopIt/ViewController.swift
+++ bopIt/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var bopItImageView: UIImageView!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        welcomeLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+
     }
 
     func die ()
